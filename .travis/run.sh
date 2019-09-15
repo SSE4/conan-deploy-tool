e5137c5a3418a7fc99b9569590ab647ca3d45157
SSE4 <tomskside@gmail.com>
2019-09-15 13:06:37 +0700

    - add travis
    Signed-off-by: SSE4 <tomskside@gmail.com>

---
+++ .travis/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    if which pyenv > /dev/null; then
+        eval "$(pyenv init -)"
+    fi
+    pyenv activate conan
+fi
+
+python setup.py sdist
+
+python setup.py install
+conan-deploy-tool --version
